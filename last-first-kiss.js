f24266229fd3914e26378994f6d61008bdaf5f68
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-20 17:06:06 +0100

    last kiss first 1

---
+++ last-first-kiss.js
@@ -0,0 +1,12 @@
+function first (liste) {
+    return liste[0]
+}
+
+function last (liste) {
+    return liste[liste.length-1]
+}
+
+function kiss (liste) {
+    liste2 = liste[0] + liste[liste.length-1]
+    return liste2
+}
