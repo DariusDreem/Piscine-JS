331f29f60373bd1ac3cd2344dff92f515383ccac
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-20 16:44:56 +0100

    more or less 1

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more (number) {
+    return number++
+}
+
+function less (number) {
+    return number--
+}
+
+function add (number1, number2) {
+    return number1+number2
+}
+
+function sub (number1,number2) {
+    return number2-number1
+}
