b752f4c1061786b3aacde9a200576078c8afe2fc
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-22 20:22:24 +0100

    method man 1

---
+++ method-man.js
@@ -0,0 +1,22 @@
+function words (phrase) {
+    return phrase.split(" ")
+}
+
+function sentence (phrase1,phrase2) {
+    phrase1 = phrase1 + phrase2
+return phrase1
+}
+
+function yell (phrase) {
+    return phrase.toUpperCase()
+}
+
+function whisper (phrase) {
+    return "*"+phrase.toLowerCase()+"*"
+}
+
+function capitalize (phrase) {
+    return phrase.charAt(0).toUpperCase() + phrase.slice(1).toLowerCase()
+}
+
+console.log(capitalize('Bonsoir Mon cheR'))
