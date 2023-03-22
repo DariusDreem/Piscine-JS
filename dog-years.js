56158b166e64ac30e8c346700cd02833a9f718fc
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-22 12:34:29 +0100

    dog years 2

--- dog-years.js
+++ dog-years.js
@@ -10,6 +10,9 @@ const planets={
 }
 
 function dogYears(planet,seconds) {
-    const hearth_year=seconds/60/60/24/365.25
-    return hearth_year / planets[planet]
+    const earth_year=(((seconds/60)/60)/24)/365.25
+    const dog_year = earth_year*7
+    return dog_year*planets[planet]
 }
+
+console.log(dogYears("earth",1000000000))
