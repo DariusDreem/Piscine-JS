51705229eb75819cc68e4962712c8553b9685849
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-23 12:40:57 +0100

    oui

--- repeat.js
+++ repeat.js
@@ -1,9 +1,10 @@
 function repeat (phrase,repet) {
     const phraseog = phrase
+    if (repet === 0){
+        return ''
+    }
     for (let i = 0; i < repet-1; i++) {
         phrase = phrase + phraseog
     }
     return phrase
 }
-
-console.log(repeat('a',3))
