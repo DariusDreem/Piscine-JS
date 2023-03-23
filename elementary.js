ae1e9ac1503402c2794ba6e2c2a175bba1b9fbc0
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-23 13:38:04 +0100

    c bon ?

--- elementary.js
+++ elementary.js
@@ -1,7 +1,15 @@
 function multiply (a,b) {
     let result = 0
+    let negative = false
+    if (b < 0){
+        b = Math.abs(b)
+        negative = true
+    }
     for (let i = 0; i < b; i++) {
         result += a
+    }
+    if (negative === true) {
+        result= result - result - result
     }
     return result
 }
@@ -21,3 +29,4 @@ function modulo (a,b) {
     let i = divide(a,b)
 return a-(multiply(i,b))
 }
+console.log(multiply(123,-22))
