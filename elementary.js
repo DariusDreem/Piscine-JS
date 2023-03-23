868181fc4f4e919f5df1b44e39384a8322aafafa
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-23 13:07:35 +0100

    elementary 1

---
+++ elementary.js
@@ -0,0 +1,23 @@
+function multiply (a,b) {
+    let result = 0
+    for (let i = 0; i < b; i++) {
+        result += a
+    }
+    return result
+}
+
+function divide (a,b) {
+    let i = 0
+    let result = 0
+    while (result === 0) {
+        i++
+        if (multiply(b,i) > a){
+            return i-1
+        }
+    }
+}
+
+function modulo (a,b) {
+    let i = divide(a,b)
+return a-(i*b)
+}
