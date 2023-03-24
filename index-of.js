5a440fed924fecfa5734656da18b95bce9374287
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-24 10:42:53 +0100

    index of 1

---
+++ index-of.js
@@ -0,0 +1,25 @@
+function indexOf (liste,arr,start) {
+    for (let i = start; i < laphrase.length-1; i++) {
+        if (liste === arr[i]){
+            return i
+        }
+    }
+}
+
+function lastIndexOf  (liste,arr,start) {
+    for (let i = start; i < laphrase.length-1; i++) {
+        if (liste === arr[i]){
+            let tempo = i
+        }
+    }
+    return tempo
+}
+
+function include (liste,arr,start) {
+    for (let i = start; i < laphrase.length-1; i++) {
+        if (liste === arr[i]){
+            return true
+        }
+    }
+    return false
+}
