818616679e155bb6e2f8a970b23273c6dccdf745
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-24 16:22:25 +0100

    oui

--- slicer.js
+++ slicer.js
@@ -1,25 +1,17 @@
 function slice(list, start, end = list.length) {
     let container;
-    if(typeof list === "string")
-    {
-        container = ""
-    }
-    else
-    {
-        container = []
+    if (typeof list === "string") {
+        container = "";
+    } else {
+        container = [];
     }
     if (start < 0) {
-        Math.abs(start)
-        for (let i = end - start; i < end; i++) {
-            container += list[i]
-        }
-        return container
-    } else {
-        for (let i = start; i < end; i++) {
-            container += list[i]
-        }
-        return container
+        start = list.length + start;
     }
+    for (let i = start; i < end; i++) {
+        container += list[i];
+    }
+    return container;
 }
 
-console.log(slice('abcdef', 2))
+console.log(slice("abcdef", -2)); // "ef"
