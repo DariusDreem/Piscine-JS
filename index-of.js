9b1ba12c58494df530f66a335382c3ceebd212c8
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-24 10:58:06 +0100

    index of 3

--- index-of.js
+++ index-of.js
@@ -1,31 +1,26 @@
-function indexOf(list, arr) {
+function indexOf(arr,list) {
     for (let i = 0; i < arr.length; i++) {
-        if (list === arr[i]) {
+        if (arr[i] === list) {
             return i;
         }
     }
 }
 
-function lastIndexOf(list, arr) {
+function lastIndexOf(arr,list) {
     let tempo = -1;
     for (let i = 0; i < arr.length; i++) {
-        if (list === arr[i]) {
+        if (arr[i] === list) {
             tempo = i;
         }
     }
     return tempo;
 }
 
-function include(list, arr) {
+function include(arr,list) {
     for (let i = 0; i < arr.length; i++) {
-        if (list === arr[i]) {
+        if (arr[i] === list) {
             return true;
         }
     }
     return false;
 }
-
-/*console.log(indexOf(2, [1, 2, 3, 4, 5, 4, 3, 2, 1]));
-console.log(lastIndexOf(2, [1, 2, 3, 4, 5, 4, 3, 2, 1]));
-console.log(include(2, [1, 2, 3, 4, 5, 4, 3, 2, 1]));
-*/
