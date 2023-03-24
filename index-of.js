fdc88524493190e8213642e0638653dc01dc4cb3
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-24 11:39:09 +0100

    oui

--- index-of.js
+++ index-of.js
@@ -1,15 +1,12 @@
-function indexOf(list, arr) {
-    if (!Array.isArray(arr)) {
-        return -1;
+function indexOf(arr,list,start) {
+    if (start === undefined) {
+        start = 0
     }
-
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = start; i < arr.length; i++) {
         if (arr[i] === list) {
             return i;
         }
     }
-
-    return -1;
 }
 
 function lastIndexOf(arr,list,start) {
