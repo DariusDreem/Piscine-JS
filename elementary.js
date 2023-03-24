7c600f4b9625c49faf2637e3366d5aef6bb5b061
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-24 10:21:35 +0100

    aled

--- elementary.js
+++ elementary.js
@@ -1,31 +1,32 @@
+// multiply function
 function multiply(a, b) {
     let result = 0;
-    if (Math.abs(b) > Math.abs(a)) {
-        [a, b] = [b, a];
-    }
     for (let i = 0; i < Math.abs(b); i++) {
         result += Math.abs(a);
     }
     return Math.sign(a) === Math.sign(b) ? result : -result;
 }
 
-
+// divide function
 function divide(dividend, divisor) {
     let quotient = 0;
     if (divisor === 0) {
-        return 0;
+        return NaN;
     }
-    while (true) {
+    while (Math.abs(dividend) >= Math.abs(divisor)) {
+        dividend = dividend - Math.abs(divisor);
         quotient++;
-        if (multiply(divisor, quotient) > dividend || quotient > 100000) {
-            return quotient - 1;
-        }
     }
+    return Math.sign(quotient) === Math.sign(divisor) ? quotient : -quotient;
 }
 
-
-function modulo(a, b) {
-    let i = divide(a, b);
-    return a - (multiply(i, b));
+// modulo function
+function modulo(dividend, divisor) {
+    if (divisor === 0) {
+        return NaN;
+    }
+    while (Math.abs(dividend) >= Math.abs(divisor)) {
+        dividend = dividend - Math.abs(divisor);
+    }
+    return Math.sign(dividend) === Math.sign(divisor) ? dividend : -dividend;
 }
-console.log(divide(123,-22))
