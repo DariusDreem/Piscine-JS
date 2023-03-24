21baf713cf45429f3bd245fe2bd0a839ac42ee4e
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-24 16:37:06 +0100

    g

--- slicer.js
+++ slicer.js
@@ -8,10 +8,15 @@ function slice(list, start, end = list.length) {
     if (start < 0) {
         start = list.length + start;
     }
+
+    if (end < 0) {
+        end = list.length - Math.abs(end)
+    }
+
     for (let i = start; i < end; i++) {
         container += list[i];
     }
     return container;
 }
 
-console.log(slice("abcdef", -2)); // "ef"
+console.log( slice('abcdef', 0, -2));
