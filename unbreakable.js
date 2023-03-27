a48f55f068c44074e6d27240e1dd4332e8da751c
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-27 16:42:34 +0200

    t

--- unbreakable.js
+++ unbreakable.js
@@ -1,21 +1,23 @@
-function split(phrase) {
+function split(phrase, motif) {
     let toreturn = [];
     for (let i = 0; i < phrase.length; i++) {
-        toreturn[i] = phrase[i];
+        if (phrase[i] !== motif) {
+            toreturn.push(phrase[i]);
+        }
     }
     return toreturn;
 }
 
 function join(list) {
-    let phrase = ""
+    let phrase = "";
     for (let i = 0; i < list.length; i++) {
-        if (i === list.length-1) {
-            phrase += list[i]
+        if (i === list.length - 1) {
+            phrase += list[i];
         } else {
-            phrase += list[i] + ", "
+            phrase += list[i] + " ";
         }
     }
-    return phrase
+    return phrase;
 }
 
-console.log(join(["bonsoir", "ça", "va ?"]));
+console.log(join(split('a b c', ' '))); // Output: "a b c"
