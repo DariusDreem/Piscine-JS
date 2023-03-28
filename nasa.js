6db8584624f8db848a3d59bcb1768596e2a471ec
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-28 12:16:59 +0200

    oui

---
+++ nasa.js
@@ -0,0 +1,21 @@
+function nasa (number) {
+    let toreturn = ""
+    let divisable = false
+    for (let i = 1; i < number; i++) {
+        if (i % 3 === 0) {
+            toreturn += "NA"
+            divisable = true
+        }
+        if (i % 5 === 0) {
+            toreturn += "SA"
+            divisable = true
+        }
+        if (divisable === false) {
+            toreturn += i
+        }
+        toreturn += " "
+        divisable = false
+    }
+    return toreturn
+}
+console.log(nasa(10))
