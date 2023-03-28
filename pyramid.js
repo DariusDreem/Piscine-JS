822125748b6101dd5b39e1289d4a6a0488b05345
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-28 16:01:42 +0200

    pyramid 2

--- pyramid.js
+++ pyramid.js
@@ -1,9 +1,13 @@
 function pyramid(motif, number) {
     let pyrami = "";
     let range = 1;
+
     for (let i = 0; i < number; i++) {
-        for (let j = 0; j < number - i - 1; j++) {
-            pyrami += " ";
+        for (let j = 2; j <= number - i; j++) { // change 'i' to 'number - i'
+            for (let k = 0; k < motif.length; k++) {
+                pyrami += " ";
+            }
+
         }
         for (let j = 0; j < range; j++) {
             pyrami += motif;
@@ -13,7 +17,8 @@ function pyramid(motif, number) {
             range += 2;
         }
     }
+
     return pyrami;
 }
 
-console.log(pyramid("#", 8));
+console.log(pyramid("{}", 5));
