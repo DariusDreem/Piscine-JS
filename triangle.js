85b81ddbdf5602df08956b25a0a604858682e2cb
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-28 14:27:19 +0200

    triangle 1

---
+++ triangle.js
@@ -0,0 +1,14 @@
+function triange (motif,number) {
+let ligneperligne = ""
+    for (let i = 1; i < number+1; i++) {
+        for (let j = 0; j < i; j++) {
+            ligneperligne += motif
+        }
+        if (i !== number) {
+            ligneperligne += "\n"
+        }
+    }
+    return ligneperligne
+}
+
+console.log(triange("*",6))
