92486d9fa5f6b261c5ceb84a607ba39210f5570e
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-28 14:04:48 +0200

    oui

--- get.js
+++ get.js
@@ -1,3 +1,11 @@
-function get (src,path) {
-    return src.path
+function get(src, path) {
+    const keys = path.split('.');
+    let value = src;
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        value = value[key];
+    }
+
+    return value;
 }
