ce67e1d71092594416ce2fde3014b78dc7360247
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-28 11:34:59 +0200

    oui

--- unbreakable.js
+++ unbreakable.js
@@ -1,22 +1,65 @@
-function split(str, separator) {
-    const arr = [];
-    let startIndex = 0;
-    let separatorIndex = str.indexOf(separator, startIndex);
-    while (separatorIndex !== -1) {
-        arr.push(str.substring(startIndex, separatorIndex));
-        startIndex = separatorIndex + separator.length;
-        separatorIndex = str.indexOf(separator, startIndex);
+function split(str, splitArg) {
+    let result = [];
+    let resultTemp = "";
+    let countToRemoveIfIsNotSplitArg = 0, i = 0;
+
+    if (splitArg === "") {
+        return justSplit(str);
     }
-    arr.push(str.substring(startIndex));
-    return arr;
+
+    for (let i = 0; i < str.length; i++) {
+        if (splitArg[0] === str[i]) {
+            for (let j = 0; j < splitArg.length; j++) {
+//                console.log("i : ", i, ", j : ", j, " : ", splitArg.length)
+                if (str[i] !== splitArg[j]) {
+                    i -= countToRemoveIfIsNotSplitArg;
+                } else if ((str[i] === splitArg[j]) && j === splitArg.length - 1) {
+                    console.log(resultTemp)
+                    if (resultTemp === "" || resultTemp === undefined) {
+                    }
+                    result.push(resultTemp)
+                    resultTemp = ""
+//                    i = i - 1;
+                } else {
+                    countToRemoveIfIsNotSplitArg++;
+                    i++
+                }
+            }
+            continue
+        }
+        resultTemp += str[i];
+    }
+
+    if (resultTemp !== 'undefined') {
+//        console.log("i : ", i)
+        result.push(resultTemp)
+    } else {
+        result.push('')
+    }
+
+
+    return result
 }
 
-function join(arr, separator) {
-    return arr.reduce((acc, curr, i) => {
-        if (i === 0) {
-            return curr;
-        } else {
-            return acc + separator + curr;
-        }
-    }, '');
+function justSplit(str) {
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        result.push(str[i])
+    }
+    return result;
 }
+
+function join(arr, joinArg) {
+    let result = "";
+    for (let i = 0; i < arr.length; i++) {
+        result = result + arr[i];
+        if (i === arr.length - 1) continue
+        result += joinArg;
+
+    }
+    return result
+}
+
+console.log(join(['ggg', 'ddd', 'b'], ' - '))
+//console.log(split("Hello - mother -   !", " - "))
+//console.log(join(['H', "E", "L", "L", "O"], " ")
