9025028743878b5bf9d9492008063c1ec540bf89
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-28 11:22:19 +0200

    unbreakable 3

--- unbreakable.js
+++ unbreakable.js
@@ -1,10 +1,11 @@
 function split(str, separator) {
     const arr = [];
     let startIndex = 0;
-    for (let i = 0; i < str.length; i++) {
+    for (let i = 0; i <= str.length - separator.length; i++) {
         if (str.substring(i, i + separator.length) === separator) {
             arr.push(str.substring(startIndex, i));
             startIndex = i + separator.length;
+            i = startIndex - 1;
         }
     }
     arr.push(str.substring(startIndex));
