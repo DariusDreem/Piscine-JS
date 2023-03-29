4efcc777b36fb09b7ab180edb5858b6517532b60
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-29 16:34:29 +0200

    vowel dots 1

---
+++ vowel-dots.js
@@ -0,0 +1,12 @@
+function vowelDots(str) {
+    const vowels = /[AEIOUaeiou]/g;
+    let newStr = '';
+    let lastIndex = 0;
+    let match;
+    while (match = vowels.exec(str)) {
+        newStr += str.slice(lastIndex, match.index + 1) + '.';
+        lastIndex = match.index + 1;
+    }
+    newStr += str.slice(lastIndex);
+    return newStr;
+}
