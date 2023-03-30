712d60042eafdfdb86a4a893ed47be4b1b45a25a
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-30 16:01:56 +0200

    same amount 3

--- same-amount.js
+++ same-amount.js
@@ -5,7 +5,4 @@ function sameAmount(str, patern1, patern2) {
         conteur2 = (str.match(patern2,'g'))
     return conteur1=== conteur2
 }
-const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
- ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
 
-console.log(!sameAmount(data, /q /, /qqqqqqq/))
