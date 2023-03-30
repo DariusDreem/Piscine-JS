736a3e92aeb54f0fbf7ede3fd0210163f4604d01
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-30 19:48:43 +0200

    ion out 2

--- ion-out.js
+++ ion-out.js
@@ -2,10 +2,11 @@ function ionOut(string) {
     let pattern = /\b\w*tion\w*\b/g;
     let toreturn = [];
     let matches = string.match(pattern);
-    for (let i = 0; i < matches.length; i++) {
-        matches[i] = matches[i].slice(0, -3);
+    if (matches !== null) {
+        for (let i = 0; i < matches.length; i++) {
+            matches[i] = matches[i].slice(0, -3);
+        }
     }
-    return matches;
+    return matches || [];
 }
 
-console.log(ionOut("I'm on a mission to find a solution for this situation"));
