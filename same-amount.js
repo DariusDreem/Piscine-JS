401a19e17dab95ae6cbda3c71a4d28ff90963e6d
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-30 16:01:16 +0200

    oui

--- same-amount.js
+++ same-amount.js
@@ -1,13 +1,11 @@
 function sameAmount(str, patern1, patern2) {
-    let conteur1 = 0
-    let conteur2 = 0
-    for (let i = 0; i < str.length; i++) {
-        if (str[i].match(patern1)) {
-            conteur1++
-        }
-        if (str[i].match(patern2)) {
-            conteur2++
-        }
-    }
-    return conteur1 === conteur2;
+    let conteur1 = []
+    let conteur2 = []
+        conteur1 = (str.match(patern1,'g'))
+        conteur2 = (str.match(patern2,'g'))
+    return conteur1=== conteur2
 }
+const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
+ ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
+
+console.log(!sameAmount(data, /q /, /qqqqqqq/))
