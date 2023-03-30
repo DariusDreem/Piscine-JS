0ce5520149c71299e866772aee9ea4b0e7933c7d
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-03-30 16:27:45 +0200

    same amount 3

--- same-amount.js
+++ same-amount.js
@@ -12,18 +12,3 @@ function sameAmount(str, patern1, patern2) {
     }
 }
 
-const data = 'qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqwijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd'
-
-
-console.log(sameAmount('hello how are you', /l/, /e/))
-console.log(sameAmount('hello how are you', /h/, /e/))
-console.log(sameAmount('hello how are you', /he/, /ho/))
-console.log(sameAmount(data, /i/, /p/))
-console.log(!sameAmount(data, /h/, /w/))
-console.log(sameAmount(data, /qqqq /, /qqqqqqq/))
-console.log(!sameAmount(data, /q /, /qqqqqqq/))
-console.log(sameAmount(data, /fs[^q]/, /q /))
-console.log(sameAmount(data, /^[qs]/, /^[gq]/))
-console.log(sameAmount(data, /j/, /w/))
-console.log(!sameAmount(data, /j/, / /))
-console.log(sameAmount(data, /fs./, /jn./))
