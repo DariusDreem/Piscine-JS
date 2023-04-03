c5f6f36fc691f7d19118f0d96fdf33e6ce7e9e04
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-03 19:04:17 +0200

    using filter 1

--- using-filter.js
+++ using-filter.js
@@ -1,18 +1,13 @@
 function filterShortStateName (string) {
 let result = string.filter(word => word.length > 7)
-    console.log(result)
     return result.length >0
 }
-
-console.log(filterShortStateName(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']))
 
 function filterStartVowel (string)  {
     const result = string.filter(word => ['a', 'e', 'i', 'o', 'u'].includes(word[0]));
-    console.log(result)
     return result.length >0
 }
 
-console.log(filterStartVowel(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']))
 
 function filter5Vowels(strings) {
     const vowels = ["a", "e", "i", "o", "u"];
@@ -22,7 +17,6 @@ function filter5Vowels(strings) {
     });
 }
 
-console.log(filter5Vowels(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']))
 
 function filter1DistinctVowel(strings) {
     const distinctVowels = ['a', 'e', 'i', 'o', 'u'];
@@ -31,8 +25,6 @@ function filter1DistinctVowel(strings) {
         return vowels.size === 1 && distinctVowels.includes(vowels.values().next().value);
     });
 }
-
-console.log(filter1DistinctVowel(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']))
 
 function multiFilter(string) {
     return string.filter(obj => (
