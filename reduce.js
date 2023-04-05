d74143fd06745568f9b59c962d5bc5c6981aeb3b
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-05 20:03:23 +0200

    reduce 2

---
+++ reduce.js
@@ -0,0 +1,39 @@
+function fold (arr,func,accumulator) {
+    for (let i = 0; i < arr.length; i++) {
+        accumulator = func(arr[i],accumulator)
+    }
+    return accumulator
+}
+
+function foldRight (arr,func,accumulator) {
+    for (let i = arr.length-1; i >= 0; i--) {
+        accumulator = func(arr[i],accumulator)
+    }
+    return accumulator
+}
+
+function reduce (arr,func,accumulator=0) {
+    if ( arr.length <= 0) {
+        throw new TypeError("Erreur : arr.length <= 0")
+    }
+    for (let i = 0; i < arr.length; i++) {
+        accumulator = func(arr[i],accumulator)
+    }
+    return accumulator
+}
+
+function reduceRight (arr,func,accumulator=0) {
+    if ( arr.length <= 0) {
+        throw new TypeError("Erreur : arr.length <= 0")
+    }
+    for (let i = arr.length-1; i >= 0; i--) {
+        accumulator = func(arr[i],accumulator)
+    }
+    return accumulator
+}
+const adder = (a, b) => a + b
+
+console.log(fold([1, 2, 3], adder, 2))
+console.log(foldRight([1, 2, 3], adder, 2))
+console.log(reduce([1, 2, 3], adder))
+console.log(reduceRight([1, 2, 3], adder))
