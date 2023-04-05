dd194a0a01f3ba6968db8201d203c105c3bfd3c5
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-06 00:57:54 +0200

    has city 1

--- has-city.js
+++ has-city.js
@@ -2,9 +2,9 @@ function hasCity(country, citys) {
     return function (city) {
         for (let i = 0; i < citys.length; i++) {
             if (citys[i] === city)
-                return city + "is a city from" + country
+                return city + " is a city from " + country
         }
-        return city + "is not a city from" + country
+        return city + " is not a city from " + country
     }
 }
 
