52ffba376dc64899a368574a932b0dd4e5356564
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-06 15:06:57 +0200

    get them all 1

---
+++ get-them-all.js
@@ -0,0 +1,27 @@
+import {people} from './get-them-all.data.js'
+
+export function getBonannoPisano(arr, el = undefined, jsp = undefined) {
+    const bonannoPisano = document.getElementById("BonannoPisano");
+    const another = Array.from(document.querySelectorAll('a:not(#BonannoPisano) span:not(#BonannoPisano)'));
+
+    console.log("B : ", bonannoPisano, "| a : ", another)
+    return [bonannoPisano, another];
+}
+
+export const getArchitects = () => {
+    const architects = Array.from(document.querySelectorAll('a'));
+    const nonArchitects = Array.from(document.querySelectorAll('span'));
+    return [architects, nonArchitects];
+};
+
+export function getActive(arr, el = undefined, jsp = undefined) {
+    const classic = Array.from(document.getElementsByClassName('active'));
+    const another = Array.from(document.querySelectorAll('a:not(.active) span:not(.active)'));
+    return [classic, another];
+}
+
+export function getClassical(arr, el = undefined, jsp = undefined) {
+    const classic = Array.from(document.getElementsByClassName('classical'));
+    const another = Array.from(document.querySelectorAll('a:not(.classical) span:not(.classical)'));
+    return [classic, another];
+}
