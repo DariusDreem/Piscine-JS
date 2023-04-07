02a32c597bb706c867fee996d3d5d0466d456d8c
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-07 16:42:22 +0200

    pimp style 2

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,54 +1,19 @@
-export const getArchitects = () => {
-    let col = document.getElementsByTagName("a");
-    let arr1 = [];
-    let arr2 = [];
-    for (let elem of col) {
-        arr1.push(elem);
-    }
-    col = document.getElementsByTagName("span");
-    for (let elem of col) {
-        arr2.push(elem);
-    }
-    console.log([arr1, arr2]);
-    return [arr1, arr2];
-};
+import { styles } from './pimp-my-style.data.js'
+const undo = 'unpimp'
 
-export const getClassical = () => {
-    let arr1 = [];
-    let arr2 = [];
-    let col = document.querySelectorAll("a.classical");
-    for (let elem of col) {
-        arr1.push(elem);
+export const pimp = () => {
+    const button = document.querySelector('button')
+    const classList = button.classList, lClassList = button.classList.length,
+        lStyles = styles.length;
+
+    classList.toggle('button',true)
+    let add = classList[lClassList-1] !== undo
+
+    if (add) {
+        classList.toggle(styles[lClassList-1],add)
+        if (classList.item(lClassList) === styles[lStyles-1]) classList.toggle(undo,true);
+    } else {
+        classList.toggle(styles[lClassList-3],add)
+        if (classList.item(lClassList-3) === 'button' && !add) classList.toggle(undo,false);
     }
-    col = document.querySelectorAll("a:not(.classical)");
-    for (let elem of col) {
-        arr2.push(elem);
-    }
-    return [arr1, arr2];
-};
-export const getActive = () => {
-    let arr1 = [];
-    let arr2 = [];
-    let col = document.querySelectorAll("a.classical.active");
-    for (let elem of col) {
-        arr1.push(elem);
-    }
-    col = document.querySelectorAll("a.classical:not(.active)");
-    for (let elem of col) {
-        arr2.push(elem);
-    }
-    return [arr1, arr2];
-};
-export const getBonannoPisano = () => {
-    let arr1 = [];
-    let arr2 = [];
-    let col = document.querySelectorAll("a#BonannoPisano.classical.active");
-    for (let elem of col) {
-        arr1.push(elem);
-    }
-    col = document.querySelectorAll("a:not(#BonannoPisano).classical.active");
-    for (let elem of col) {
-        arr2.push(elem);
-    }
-    return [arr1[0], arr2];
-};
+}
