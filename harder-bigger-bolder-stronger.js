aa9c88f98b3c14314ffaacd7dc6b9e1b5f82d1be
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-07 15:39:33 +0200

    harder bigger

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,24 @@
+export const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+export const generateLetters = () => {
+    let last = document.querySelector("body");
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+    for (let ind = 0; ind < 120; ind++) {
+        const newDiv = document.createElement("div");
+        newDiv.textContent = alphabet[getRandomNumber(0, 25)];
+        if (ind < 40) {
+            newDiv.style.cssText =
+                "font-size: " + (11 + ind) + "px; font-weight: " + 300 + ";";
+        } else if (ind < 80) {
+            newDiv.style.cssText =
+                "font-size: " + (11 + ind) + "px; font-weight: " + 400 + ";";
+        } else {
+            newDiv.style.cssText =
+                "font-size: " + (11 + ind) + "px; font-weight: " + 600 + ";";
+        }
+        last.append(newDiv);
+    }
+};
