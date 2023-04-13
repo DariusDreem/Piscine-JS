4675d130007bb3b743831ee4e9ed92632247d1a3
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-13 15:35:33 +0200

    invert 1

---
+++ invert.js
@@ -0,0 +1,10 @@
+
+function invert(obj) {
+    const inverted = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            inverted[obj[key]] = key;
+        }
+    }
+    return inverted;
+}
