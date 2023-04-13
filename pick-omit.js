f9b4b6c310d584c96a3cda5d7424c3f2aa8fcacc
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-13 16:01:30 +0200

    pick omit 2

---
+++ pick-omit.js
@@ -0,0 +1,20 @@
+
+function pick(obj, keys) {
+    const picked = {};
+    for (const key of keys) {
+        if (obj.hasOwnProperty(key)) {
+            picked[key] = obj[key];
+        }
+    }
+    return picked;
+}
+
+function omit(obj, keys) {
+    const omitted = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key) && !keys.includes(key)) {
+            omitted[key] = obj[key];
+        }
+    }
+    return omitted;
+}
