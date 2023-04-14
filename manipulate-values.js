a5bf096d3fb2cbf82842b4a501bf64c2d8634110
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-04-14 15:22:44 +0200

    oui

---
+++ manipulate-values.js
@@ -0,0 +1,30 @@
+function reduce(array, func) {
+    if (array.length < 1) {
+        throw new Error("Array must have at least one element");
+    }
+    let accumulator = array[0];
+    for (let i = 1; i < array.length; i++) {
+        accumulator = func(accumulator, array[i]);
+    }
+    return accumulator;
+}
+
+function filter(array, callback) {
+    const filteredArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            filteredArray.push(array[i]);
+        }
+    }
+
+    return filteredArray;
+}
+
+function map(array, callback) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i], i, array));
+    }
+    return result;
+}
